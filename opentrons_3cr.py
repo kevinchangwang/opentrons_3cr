c16e06590ac8cd04376164c72a47889f8669c974
Kevin Wang <kevinchangwang@gmail.com>
2022-08-16 18:24:38 -0400

    Changed the volume of reagents to 20uL for only one plate per run. Also fixed error with pipette tip pick up.

--- opentrons_3cr.py
+++ opentrons_3cr.py
@@ -20,12 +20,6 @@ A_Component_Dict = {
     'B4': 'H'
 }
 
-# Specifies the transfer of B components from the source plate well to destination plate
-B_Component_Dict = {
-    'C3': '1',
-    'C4': '2'
-}
-
 # Specifies the transfer of C components from the source plate well to destination plate columns
 C_Component_Dict = {
     'A4': '1',
@@ -46,43 +40,38 @@ C_Component_Dict = {
 # protocol run function
 def run(protocol: protocol_api.ProtocolContext):
     # labware
-    solvent_plate = protocol.load_labware('opentrons_15_tuberack_falcon_15ml_conical', location='5',
+    solvent_plate = protocol.load_labware('usascientific_12_reservoir_22ml', location='5',
                                           label='Solvent_Plate')
     source_plate = protocol.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', location='4',
                                          label='Source_Plate')
-    destination_plate_1 = protocol.load_labware('sarstedt_96_wellplate_200ul', location='1',
-                                                label='Destination_Plate_1')
-    destination_plate_2 = protocol.load_labware('sarstedt_96_wellplate_200ul', location='2',
-                                                label='Destination_Plate_2')
-    tiprack_1 = protocol.load_labware('opentrons_96_tiprack_20ul', location='7', label='Tip_Rack_1')
-    tiprack_2 = protocol.load_labware('opentrons_96_tiprack_20ul', location='8', label='Tip_Rack_2')
-    tiprack_3 = protocol.load_labware('opentrons_96_tiprack_20ul', location='9', label='Tip_Rack_3')
+    destination_plate = protocol.load_labware('sarstedt_96_wellplate_200ul', location='1',
+                                              label='Destination_Plate_1')
+    tiprack_1 = protocol.load_labware('opentrons_96_tiprack_300ul', location='7', label='Tip_Rack_1')
+    tiprack_2 = protocol.load_labware('opentrons_96_tiprack_300ul', location='8', label='Tip_Rack_2')
+    tiprack_3 = protocol.load_labware('opentrons_96_tiprack_300ul', location='9', label='Tip_Rack_3')
     tiprack_4 = protocol.load_labware('opentrons_96_tiprack_300ul', location='10', label='Tip_Rack_4')
     # pipette
-    right_pipette = protocol.load_instrument('p20_single_gen2', mount='right',
+    right_pipette = protocol.load_instrument('p300_single_gen2', mount='right',
                                              tip_racks=[tiprack_1, tiprack_2, tiprack_3])
-    left_pipette = protocol.load_instrument('p300_single_gen2', mount='left',
+    left_pipette = protocol.load_instrument('p300_multi_gen2', mount='left',
                                             tip_racks=[tiprack_4])
-    # transfer 87.5ul of solvent to both destination plates
-    left_pipette.distribute(87.5, solvent_plate.well('A1'), destination_plate_1.wells(), new_tip='never')
-    # left_pipette.drop_tip()
-    left_pipette.distribute(87.5, solvent_plate.well('A2'), destination_plate_2.wells(), new_tip='never')
-    # left_pipette.drop_tip()
+    # transfer 57.5ul of solvent to both destination plates
+    left_pipette.pick_up_tip()
+    left_pipette.distribute(57.5, solvent_plate.well('A1'), destination_plate.wells(), new_tip='never')
+    left_pipette.drop_tip()
 
     # distribute A components to the designated rows
+    right_pipette.pick_up_tip()
     for component in A_Component_Dict:
-        right_pipette.transfer(10, source_plate.wells_by_name()[component],
-                               destination_plate_1.rows_by_name()[A_Component_Dict[component]], new_tip='always')
-        right_pipette.transfer(10, source_plate.wells_by_name()[component],
-                               destination_plate_2.rows_by_name()[A_Component_Dict[component]], new_tip='always')
+        right_pipette.transfer(20, source_plate.wells_by_name()[component],
+                               destination_plate.rows_by_name()[A_Component_Dict[component]], new_tip='always')
 
     # distribute B components to the designated plates
-    right_pipette.transfer(10, source_plate.wells_by_name()['C3'], destination_plate_1.wells(), new_tip='always')
-    right_pipette.transfer(10, source_plate.wells_by_name()['C4'], destination_plate_2.wells(), new_tip='always')
+    right_pipette.transfer(20, source_plate.wells_by_name()['C3'], destination_plate.wells(), new_tip='always')
 
     # distribute C components to the designated columns
     for component in C_Component_Dict:
-        right_pipette.transfer(10, source_plate.wells_by_name()[component],
-                               destination_plate_1.columns_by_name()[A_Component_Dict[component]], new_tip='always')
-        right_pipette.transfer(10, source_plate.wells_by_name()[component],
-                               destination_plate_2.columns_by_name()[A_Component_Dict[component]], new_tip='always')
+        right_pipette.transfer(20, source_plate.wells_by_name()[component],
+                               destination_plate.columns_by_name()[A_Component_Dict[component]], new_tip='always')
+
+    right_pipette.drop_tip()
