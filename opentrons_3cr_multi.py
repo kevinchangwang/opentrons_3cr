1e154eb001fef0b2f425ee4745340cbd13b47081
Kevin Wang <kevinchangwang@gmail.com>
2022-08-18 10:54:37 -0400

    Bug fixes, no errors upon importing into Opentrons app

--- opentrons_3cr_multi.py
+++ opentrons_3cr_multi.py
@@ -39,18 +39,18 @@ A_Component_Dict = {
 
 # Specifies the transfer of C components (lipid tails) from the source plate well to destination plate columns
 C_Component_Dict = {
-    'A4': '1',
-    'B4': '2',
-    'C4': '3',
-    'D4': '4',
+    'A1': '1',
+    'A2': '2',
+    'A3': '3',
+    'A4': '4',
     'A5': '5',
-    'B5': '6',
-    'C5': '7',
-    'D5': '8',
-    'A6': '9',
-    'B6': '10',
-    'C6': '11',
-    'D6': '12'
+    'A6': '6',
+    'A7': '7',
+    'A8': '8',
+    'A9': '9',
+    'A10': '10',
+    'A11': '11',
+    'A12': '12'
 }
 
 
@@ -73,19 +73,23 @@ def run(protocol: protocol_api.ProtocolContext):
     # pipette
     p300_single_pipette = protocol.load_instrument('p300_single_gen2', mount='right',
                                                    tip_racks=[tiprack_1, tiprack_2, tiprack_3])
-    p300_multi_pipette = protocol.load_instrument('p300_multi_gen2', mount='right',
+    p300_multi_pipette = protocol.load_instrument('p300_multi_gen2', mount='left',
                                                   tip_racks=[tiprack_1, tiprack_2, tiprack_3])
 
     # distribute B components to an empty plate
+    p300_multi_pipette.pick_up_tip()
     p300_multi_pipette.transfer(component_volume, linker_source_plate.wells_by_name()['A1'], destination_plate.wells(),
                                 new_tip='never')
+    p300_multi_pipette.drop_tip()
 
     # distribute A components to the designated rows
+
     for component in A_Component_Dict:
         p300_single_pipette.transfer(component_volume, head_source_plate.wells_by_name()[component],
                                      destination_plate.rows_by_name()[A_Component_Dict[component]], new_tip='always')
 
     # distribute C components to the designated columns
+
     for component in C_Component_Dict:
         p300_multi_pipette.transfer(component_volume, tail_source_plate.wells_by_name()[component],
                                     destination_plate.columns_by_name()[C_Component_Dict[component]], new_tip='always')
