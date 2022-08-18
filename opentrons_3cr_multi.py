31b28dc14646d61a014241585b6407525fd68f68
Kevin Wang <kevinchangwang@gmail.com>
2022-08-18 14:28:28 -0400

    Changed the multichannel version to take from a 96 deepwell source plate

--- opentrons_3cr_multi.py
+++ opentrons_3cr_multi.py
@@ -9,33 +9,24 @@ metadata = {
 }
 
 # This protocol will use the Opentrons OT-2 robot to combine the 3 components for the 3 component reaction
-# Version: Multi-channel pipette and single-channel pipette
+# Version: Multi-channel pipette
 
 # Set the component volume here
 component_volume = 40
 
 # Change the location of the plates here
-head_source_plate_location = '8'
-linker_source_plate_location = '7'
+linker_head_source_plate_location = '7'
 tail_source_plate_location = '10'
 destination_plate_location = '11'
+
+# Load component_volume*12 into the specified columns in the linker_head_source_plate
+head_component_col = '1'
+linker_component_col = '2'
 
 # This protocol requires 3 pipette racks, 1 for each component. Indicate the locations here
 tip_rack_1_location = '4'
 tip_rack_2_location = '5'
 tip_rack_3_location = '6'
-
-# Specifies the transfer of A components (head group) from the source plate well to destination plate rows
-A_Component_Dict = {
-    'A1': 'A',
-    'B1': 'B',
-    'C1': 'C',
-    'D1': 'D',
-    'A2': 'E',
-    'B2': 'F',
-    'C2': 'G',
-    'D2': 'H'
-}
 
 # Specifies the transfer of C components (lipid tails) from the source plate well to destination plate columns
 C_Component_Dict = {
@@ -54,16 +45,18 @@ C_Component_Dict = {
 }
 
 
+# This protocol will use add 1 linker to all the wells from the designated column of the linker_head_source_plate
+# 8 head groups will be distributed into the designated column of the linker_head_source_plate
+# 12 tail groups will be added to the designated column of the plate
+
 # protocol run function
 def run(protocol: protocol_api.ProtocolContext):
     # labware
     tail_source_plate = protocol.load_labware('usascientific_12_reservoir_22ml', location=tail_source_plate_location,
                                               label='Tail_Source_Plate')
-    head_source_plate = protocol.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap',
-                                              location=head_source_plate_location,
-                                              label='Head_Source_Plate')
-    linker_source_plate = protocol.load_labware('usascientific_12_reservoir_22ml',
-                                                location=linker_source_plate_location, label='Linker_Source_Plate')
+    linker_head_source_plate = protocol.load_labware('usascientific_12_reservoir_22ml',
+                                                     location=linker_head_source_plate_location,
+                                                     label='Linker_Source_Plate')
 
     destination_plate = protocol.load_labware('sarstedt_96_wellplate_200ul', location='11',
                                               label='Destination_Plate')
@@ -71,22 +64,18 @@ def run(protocol: protocol_api.ProtocolContext):
     tiprack_2 = protocol.load_labware('opentrons_96_tiprack_300ul', location=tip_rack_2_location, label='Tip_Rack_2')
     tiprack_3 = protocol.load_labware('opentrons_96_tiprack_300ul', location=tip_rack_3_location, label='Tip_Rack_3')
     # pipette
-    p300_single_pipette = protocol.load_instrument('p300_single_gen2', mount='right',
-                                                   tip_racks=[tiprack_1, tiprack_2, tiprack_3])
     p300_multi_pipette = protocol.load_instrument('p300_multi_gen2', mount='left',
                                                   tip_racks=[tiprack_1, tiprack_2, tiprack_3])
-
-    # distribute B components to an empty plate
+    # distribute head components to an empty plate
     p300_multi_pipette.pick_up_tip()
-    p300_multi_pipette.transfer(component_volume, linker_source_plate.wells_by_name()['A1'], destination_plate.wells(),
-                                new_tip='never')
+    p300_multi_pipette.transfer(component_volume, linker_head_source_plate.columns_by_name()[head_component_col],
+                                destination_plate.wells(), new_tip='never')
     p300_multi_pipette.drop_tip()
 
-    # distribute A components to the designated rows
-
-    for component in A_Component_Dict:
-        p300_single_pipette.transfer(component_volume, head_source_plate.wells_by_name()[component],
-                                     destination_plate.rows_by_name()[A_Component_Dict[component]], new_tip='always')
+    # distribute linker components to an empty plate
+    p300_multi_pipette.transfer(component_volume, linker_head_source_plate.columns_by_name()[linker_component_col],
+                                destination_plate.wells(),
+                                new_tip='always')
 
     # distribute C components to the designated columns
 
