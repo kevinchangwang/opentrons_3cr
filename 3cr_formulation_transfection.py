2b04b94eb7d8c0da77db4ec8f4681080c2f0a8c8
Kevin Wang <kevinchangwang@gmail.com>
2022-09-06 10:48:04 -0400

    3cr_formulation_transfection.py passed opentrons app check

--- 3cr_formulation_transfection.py
+++ 3cr_formulation_transfection.py
@@ -30,22 +30,22 @@ cell_plate_location = '9'
 def run(protocol: protocol_api.ProtocolContext):
     # Load Labware
     # Load Plates
-    master_mix_source_plate = protocol.load_labware('pcr_96_wellplate_singlep20',
+    master_mix_source_plate = protocol.load_labware('vwr_96_pcr_wellplate_200ul',
                                                     location=master_mix_source_plate_location,
                                                     label='Master_Mix_Source_Plate')
     ionizable_lipid_source_plate = protocol.load_labware('sarstedt_96_wellplate_200ul',
                                                          location=ionizable_lipid_source_plate_location,
                                                          label='Ionizable_Lipid_Plate')
-    nanoparticle_plate = protocol.load_labware('pcr_96_wellpate_singlep20', location=nanoparticle_plate_location,
+    nanoparticle_plate = protocol.load_labware('vwr_96_pcr_wellplate_200ul', location=nanoparticle_plate_location,
                                                label='Nanoparticle_Destination_Plate')
-    mrna_source_plate = protocol.load_labware('pcr_96_wellpate_singlep20', location=mrna_plate_location,
+    mrna_source_plate = protocol.load_labware('vwr_96_pcr_wellplate_200ul', location=mrna_plate_location,
                                               label='mRNA_Plate')
-    cell_plate = protocol.load_labware('pcr_96_wellpate_singlep20', location=cell_plate_location, label='Cell_Plate')
+    cell_plate = protocol.load_labware('vwr_96_pcr_wellplate_200ul', location=cell_plate_location, label='Cell_Plate')
 
     # Load Tipracks
     tiprack_1 = protocol.load_labware('opentrons_96_tiprack_20ul', location=tip_rack_1_location, label='Tip_Rack_1')
     tiprack_2 = protocol.load_labware('opentrons_96_tiprack_20ul', location=tip_rack_2_location, label='Tip_Rack_2')
-    tiprack_3 = protocol.load_labware('opentrons_96_tiprack_20ul', location=tip_rack_2_location, label='Tip_Rack_2')
+    tiprack_3 = protocol.load_labware('opentrons_96_tiprack_20ul', location=tip_rack_3_location, label='Tip_Rack_2')
     tiprack_4 = protocol.load_labware('opentrons_96_tiprack_300ul', location=tip_rack_4_location, label='Tip_Rack_4')
     # Load Pipettes
     p20_multi_pipette = protocol.load_instrument('p20_multi_gen2', mount='left',
@@ -53,8 +53,10 @@ def run(protocol: protocol_api.ProtocolContext):
     p300_multi_pipette = protocol.load_instrument('p300_multi_gen2', mount='right', tip_racks=[tiprack_4])
 
     # Add master mix to empty PCR plate
+    p20_multi_pipette.pick_up_tip()
     p20_multi_pipette.transfer(lipid_master_mix_amount, master_mix_source_plate.columns_by_name()[master_mix_col],
                                nanoparticle_plate.wells(), new_tip='never')
+    p20_multi_pipette.drop_tip()
 
     # Add ionizable lipid to PCR plate and mix
     p20_multi_pipette.transfer(ionizable_lipid_amount, ionizable_lipid_source_plate.wells(),
